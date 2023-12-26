01550d43787c6de1ddb4d648596a8a04fef90fa1
Gokul Kumar Jayaram <gokulkumar.jayaram@ad.infosys.com>
2023-12-26 00:14:41 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python_packaging
-Steps to package your python module and make it pip installable
+
+Pyhton module created as part of a tutorial for packaging python scripts pip installable.
+
+
+## Examples of How To Use gktools
+
+```python
+from gktools import calculator
+
+calculator.add(2,3)
+calculator.subtract(5,3)
+calculator.multiply(2,3)
+calculator.divide(4,2)
+calculator.modulo(4,3)
+```
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'gktools is an assiting package for doing basic math operations'
+LONG_DESCRIPTION = 'A package that allows to basic math operations. This package is built as part of a tutorial for packaging phython module'
+
+# Setting up
+setup(
+    name="gktools",
+    version=VERSION,
+    author="Gokul Kumar Jayaram",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'gktools', 'packaging' , 'baisc math'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
